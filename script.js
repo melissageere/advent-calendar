6418c5a7fc19de30a89e43abd7d64631938acd36
melissageere <56969511+melissageere@users.noreply.github.com>
2019-12-01 21:45:26 +0000

    hopefully final commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var messageList = ["Dearest darling Nicola, Welcome to your personalised advent calendar! To start off the festive countdown, we would like to cordially invite you to a carol service this evening at Our Lady of Lourdes Primary School in Rottingdean at 5pm. Enjoy!", "For Day 2, some Christmas craft porn to watch on the bus...", "This time 2 years ago exactly! How tiny!", "Remember this? A little Christmas throwback to get you in the mood for decoration making...", "Let's dive in to advent with this fun Isla and Shorba video", "Christmas Isla", "Christmas Shorba", "We have spent so many Christmases together! Remember this one where we were barely talking but still managed to cook dinner for everyone?", "Look at these two cuties getting their Christmas on...", "Today we want to make you feel extra-special. If you look in the kitchen you will find a Christmas treat just for you <3", "11", "Merry Christmas from the world's cutest couple!", "Christmas Julian", "Did you remember that we all had Christmas dinner together in 2013???", "I'm dreaming of a white Christmas...with every snowball that I eat...", "Happy Christmas Countdown! Except, today is not about you. So don't forget to wish this one a happy birthday today (and then we can all get back to looking forward to Christmas again)", "Christmas Adrian", "18", "19", "A screenshot of our Whatsapp conversation on this day in 2015. Lucky we did eventually find more time to share!", "You're going home today! Thank goodness, now Christmas can properly begin (think Isla and Shorba)", "22", "Two days till Christmassssss!", "One day til Christmasssss!", "HAPPY CHRISTMASSSSSSSS! You made it! Wishing you a very merry Christmas and lots of love from us both"]
+  var messageList = ["Dearest darling Nicola, Welcome to your personalised advent calendar! To start off the festive countdown, we would like to cordially invite you to a carol service this evening at Our Lady of Lourdes Primary School in Rottingdean at 5pm. Enjoy!", "For Day 2, some Christmas craft porn to watch on the bus...", "This time 2 years ago exactly! How tiny!", "Remember this? A little Christmas throwback to get you in the mood for decoration making...", "Let's dive in to advent with this fun Isla and Shorba video", "Christmas Isla", "Christmas Shorba", "We have spent so many Christmases together! Remember this one where we were barely talking but still managed to cook dinner for everyone?", "Look at these two cuties getting their Christmas on...", "Today we want to make you feel extra-special. If you look in the kitchen you will find a Christmas treat just for you <3", "Today, Melissa is going to write a Christmas poem for you. Don't forget to remind her!", "Merry Christmas from the world's cutest couple!", "Christmas Julian", "Did you remember that we all had Christmas dinner together in 2013???", "I'm dreaming of a white Christmas...with every snowball that I eat...", "Happy Christmas Countdown! Except, today is not about you. So don't forget to wish this one a happy birthday today (and then we can all get back to looking forward to Christmas again)", "Christmas Adrian", "Memories of our first Christmas together (in February!)", "Hope you get treated with plenty of R-E-S-P-E-C-T this Christmas", "A screenshot of our Whatsapp conversation on this day in 2015. Lucky we did eventually find more time to share!", "You're going home today! Thank goodness, now Christmas can properly begin (think Isla and Shorba)", "Jingle bells, jingle bells, jingle all the way!", "Two days till Christmassssss!", "One day til Christmasssss!", "HAPPY CHRISTMASSSSSSSS! You made it! Wishing you a very merry Christmas and lots of love from us both"]
 
   var treatList = ["advent calendar/images/1st.JPG", "https://www.youtube.com/embed/aGoZ9GkH_AE", "advent calendar/images/3rd.jpg", "advent calendar/images/4th.docx", "advent calendar/images/5th.mp4", "advent calendar/images/6th.jpg", "advent calendar/images/7th.jpg", "advent calendar/images/8th.jpg", "advent calendar/images/9th.jpg", "advent calendar/images/10th.JPG", "advent calendar/images/11th.JPG","advent calendar/images/12th.jpg", "advent calendar/images/13th.jpg", "advent calendar/images/14th.jpg", "advent calendar/images/15th.mp4", "advent calendar/images/16th.jpg", "./advent calendar/images/17th.jpg", "advent calendar/images/18th.jpg", "advent calendar/images/19th.JPG", "advent calendar/images/20th.jpg", "advent calendar/images/21st.jpg", "advent calendar/images/22nd.JPG", "advent calendar/images/23rd.JPG", "advent calendar/images/24th.JPG", "advent calendar/images/25th.JPG"]
 
@@ -10,14 +10,14 @@ $(document).ready(function(){
 
    function checkPermission(doornum) {
      date = new Date().getDate();
-    /* if (date < doornum) {
+    if (date < doornum) {
        alert ("No peeking!");
-     } else { */
+     } else {
          var index = doornum -1
        alert (messageList[index]);
          popUp(index);
          $(doorList[index]).fadeTo(2000, 0);
-     /*}*/
+     }
     }
 
    function popUp(doornum) {
